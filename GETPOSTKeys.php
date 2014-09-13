baa7f89c4399f629b993370ee3f1c62be87ebdcd
Chris P <chris.pate@withit.com>
2014-09-13 08:02:22 -0400

    Simplified get post keys update

--- GETPOSTKeys.php
+++ GETPOSTKeys.php
@@ -11,7 +11,7 @@ $db = connectToDb();
 include_once('getFiles/is_date.php');
 include_once('getFiles/is_datetime.php');
 
-foreach ($_POST as $key=>$value)
+foreach ($_REQUEST as $key=>$value)
 {
 	$tempKey = strtolower($key);
 	$key = strToLower($key);
@@ -81,79 +81,4 @@ foreach ($_POST as $key=>$value)
 		}
 	}
 }
-
-foreach ($_GET as $key=>$value)
-{
-	$key = strToLower($key);
-
-	if (is_date($value))
-	{
-		$$key =  date('Y-m-d', strToTime($value));
-		if (strstr($key, '_'))
-		{
-			$key = str_replace('_', '', $key);
-			$key =  date('Y-m-d H:i:s', strToTime($value));
-		}		
-	}
-	else
-	if (is_datetime($value))
-	{
-		$$key =  date('Y-m-d H:i:s', strToTime($value));
-		if (strstr($key, '_'))
-		{
-			$key = str_replace('_', '', $key);
-			$$key =  date('Y-m-d H:i:s', strToTime($value));
-		}
-	}
-	else
-	if (is_numeric($value) && strlen($value) < 10)
-	{
-		if (!strstr($value, '.'))
-		{
-			$startLength = strlen($value);
-			//value is an integer
-			$value = intval($value);
-			if (strlen($value) < $startLength)
-			{
-				for ($i=strlen($value); $i<$startLength; $i++)
-				{
-					$value = '0'.$value;
-				}
-			}
-			$$key = $value;
-			if (strstr($key, '_'))
-			{
-				$key = str_replace('_', '', $key);
-				$$key = $value;
-			}
-		}
-		else
-		{
-			//value is a float
-			$$key = floatval($value);
-			if (strstr($key, '_'))
-			{
-				$key = str_replace('_', '', $key);
-				$$key = floatval($value);
-			}
-		}
-	}
-	else
-	{
-		//value is a string
-		$key = strToLower($key);
-		$value = strToLower($value);
-	
-		$$key = mysql_real_escape_string($value);
-
-		if (strstr($key, '_'))
-		{
-			$key = str_replace('_', '', $key);
-			$$key = mysql_real_escape_string($value);
-		}
-	}
-}
-
-
-
 ?>
